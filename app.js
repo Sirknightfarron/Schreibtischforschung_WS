8a0d7cf52f6359419410927f0d666cd1d5ee6a78
SirKnightFarron <mt@adepto.as>
2021-02-05 00:57:55 +0100

    add click functionality and some new styles

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
+
 const app = Vue.createApp({
     data() {
         return {
             inventory: [],
         }
     },
+    computed: {},
     methods: {
         testclick () {
             console.log("Hit");
             alert("Kaffetasse click");
         },
+        inventoryEmpty () {
+            if(this.inventory.length == 0) {
+                return true;
+            }
+            return false;
+        },
         addItem(item) {
             switch (item) {
-                 case "kaffetasse":
+                case "kaffetasse":
                     this.inventory.unshift({
+                        id: "kaffetasse",
                         was: "Die dunkle Versuchung",
-                        text: "Die Schwarze Versuchung - Der dunkle saft, der alles im Gleichgewicht hält, aber auch alles aus dem Gleichgewicht bringen kann.",
-                        image: "src"
+                        text: "Die Schwarze Versuchung - Der dunkle Saft, der alles im Gleichgewicht hält, aber auch alles aus dem Gleichgewicht bringen kann.",
+                        image: "/images/Kaffeetasse_icon.png"
                     })
                     break;
                 case "minzmönch":
                     this.inventory.unshift({
+                        id: "minzmönch",
                         was: "Der Minzmönch",
-                        text: "Der Wächter des Turmes und des dunklen Saftes",
-                        image: "src"
+                        text: `Der Wächter des Turmes und des dunklen Saftes.
+                        Nicht viel ist über ihn bekannt, er scheint aber schon älter zu sein,
+                        als man vermuten mag. Auch nach all den Jahren ist seine Kampfkunst ungeschlagen.
+                        `,
+                        image: "/images/Minzmönch_icon.png"
                     })
                     break;
                 default:
