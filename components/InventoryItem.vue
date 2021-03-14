09bf76e7f2ec7d1286f54efc30c7496158086f63
SirKnightFarron <mt@adepto.as>
2021-03-14 22:32:42 +0100

    add first steps for refactoring into better vue

---
+++ components/InventoryItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <li class="inv-item">
+        <img class="item-icon" :src="item.image">
+        <h3>{{ item.was }}</h3>
+        <h4>{{ item.text }}</h4>
+        <audio :src="item.voice" controls="true" controlslist="nodownload "></audio>
+    </li>
+</template>
+
+<script>
+export default {
+    props: [
+        'item'
+    ],
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue';
+
+import App from './app.js'
+// import modalTemplate from './components/modalTemplate.vue'
+// import InventoryItem from './components/InventoryItem.vue'
+
+const app = createApp(App);
+
+
+// app.component('inventory-item', InventoryItem);
+
+app.mount('#werkstueck');
